0afb354782a66749a31fc165ce975735a187eaa0
rjsdn031 <rjsdn031@naver.com>
2024-05-26 16:38:00 +0900

    temp

--- matzip/src/App.jsx
+++ matzip/src/App.jsx
@@ -1,41 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import Post from './post/Post';
-import info from './assets/info.json'
-import './App.css';
+import PostForm from "./components/PostForm/PostForm"
+import PostList from "./components/PostList/PostList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {info.map((post, index) => (
-        <Post
-          key={index}
-          title={post.title}
-          // src={post.src}
-          article={post.article}
-        />
-      ))}
-
-      {
-        // /* API 호출 후 받아오기 */
-        // useEffect(() => {
-        //   fetch(/* api url */)
-        //     .then(response => {
-        //       if (response.ok) {
-        //         console.log(response);
-        //         return response.json();
-        //       }
-        //       throw new Error('Network response was not ok.');
-        //     }).then(jsonData => {
-        //       setData(jsonData);
-        //     }).catch(error => {
-        //       console.error('There was been a problem with your fetch operation: ', error); // 오류 처리
-        //     });
-        // }, []);
-      }
+      <PostForm />
+      <PostList />
     </>
-  )
-};
+  );
+}
 
 export default App;
---
+++ matzip/src/func/request.js
@@ -0,0 +1,12 @@
+import { baseUrl } from "../config/const";
+import { postsAtom, store } from "../state/atom";
+
+export function getPosts() {
+    store.set(postsAtom, async () => {
+        const res = await fetch(`${baseUrl}/posts`); // default: get
+        const result = await res.json();
+        result.sort((a, b) => b.id - a.id);   // sort by newest
+        return result;
+
+    });
+}
