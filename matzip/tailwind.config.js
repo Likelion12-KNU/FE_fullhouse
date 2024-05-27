7527c880eab59224f939d79fd09e9bbf589c3810
rjsdn031 <rjsdn031@naver.com>
2024-05-27 14:11:37 +0900

    lib reinstall/mod local test json

--- matzip/tailwind.config.js
+++ matzip/tailwind.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// tailwind 비활성화 240526
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
