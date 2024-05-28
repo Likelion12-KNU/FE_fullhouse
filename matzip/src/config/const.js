c3c3d8f5541ce55f0095a9ce4a3da400c37a44a7
rjsdn031 <rjsdn031@naver.com>
2024-05-28 19:26:02 +0900

    api-connect

--- matzip/src/config/const.js
+++ matzip/src/config/const.js
@@ -1,1 +1,1 @@
-export const baseUrl = 'http://localhost:3001';
+export const baseUrl = 'http://lion-matzip.duckdns.org/api';
--- matzip/src/func/request.js
+++ matzip/src/func/request.js
@@ -3,10 +3,9 @@ import { postsAtom, store } from "../state/atom";
 
 export function getPosts() {
     store.set(postsAtom, async () => {
-        const res = await fetch(`${baseUrl}/posts`); // default: get
+        const res = await fetch(`${baseUrl}/boards`); // default: get
         const result = await res.json();
         result.sort((a, b) => b.id - a.id);   // sort by newest
         return result;
-
     });
 }
